579fdab948a405d0046c7bad9147f694d023e5bf
lecion <ylc931116@gmail.com>
2015-05-23 15:07:36 +0800

    插件化开发

--- pluginone/src/main/java/com/yliec/pluginone/PluginClass.java
+++ pluginone/src/main/java/com/yliec/pluginone/PluginClass.java
@@ -2,7 +2,7 @@ package com.yliec.pluginone;
 /**
  * Created by Lecion on 5/22/15.
  */
-public class PluginClass {
+public class PluginClass{
     public int add(int a, int b) {
         return a + b;
     }
