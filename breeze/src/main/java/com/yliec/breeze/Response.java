be20c5caac84de8c58fc196d68928cd43a36c04d
lecion <ylc931116@gmail.com>
2015-06-16 12:07:31 +0800

    完成Response

--- breeze/src/main/java/com/yliec/breeze/Response.java
+++ breeze/src/main/java/com/yliec/breeze/Response.java
@@ -1,8 +1,62 @@
 package com.yliec.breeze;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.ReasonPhraseCatalog;
+import org.apache.http.StatusLine;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.Locale;
+
 /**
  * Created by Lecion on 6/14/15.
  */
-public class Response<T> {
+public class Response<T> extends BasicHttpResponse{
+
+    byte[] rawData = new byte[0];
+
+    public Response(StatusLine statusline, ReasonPhraseCatalog catalog, Locale locale) {
+        super(statusline, catalog, locale);
+    }
+
+    public Response(StatusLine statusline) {
+        super(statusline);
+    }
+
+    public Response(ProtocolVersion ver, int code, String reason) {
+        super(ver, code, reason);
+    }
+
+    public void setEntity(HttpEntity entity) {
+        super.setEntity(entity);
+        rawData = entityToBytes(entity);
+    }
+
+    private byte[] entityToBytes(HttpEntity entity) {
+        try {
+            return EntityUtils.toByteArray(entity);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
+    }
+
+    public byte[] getRawData() {
+        return rawData;
+    }
+
+    public void setRawData(byte[] rawData) {
+        this.rawData = rawData;
+    }
+
+    public int getStatusCode() {
+        return getStatusLine().getStatusCode();
+    }
+
+    public String getMessage() {
+        return getStatusLine().getReasonPhrase();
+    }
 
 }
